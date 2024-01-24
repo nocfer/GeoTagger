8f228d92db43cee1a7b33988308cefd1e86bbe54
Ferdinando Nocera <nocera.ferdinando@gmail.com>
2024-01-24 10:58:45 +0100

    fix: pass arguments correctly

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -8,8 +8,8 @@ program
   .description(
     'Geotag photos using a gpx file. The programs looks up the closest gpx log time for each photo creation time and then tags it with the gps coordinates from the gpx file'
   )
-  .requiredOption('-d, --photos-dir <dirPath>', 'The directory path of the photos to tag')
+  .argument('<dir>', 'The directory path of the photos to tag')
   .requiredOption('-gpx, --gpx <gpxPath>', 'The path to the gpx file to use to geotag the photos')
-  .action((dirPath: string, gpxFilePath: string) => geotag(dirPath, gpxFilePath));
+  .action((dirPath, options) => geotag(dirPath, options.gpx));
 
 export const start = () => program.parse();
